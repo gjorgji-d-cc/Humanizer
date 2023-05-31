64ae3206e924522605d7806bea6b0cbeebf8196c
Ljupce <ljupce@halicea.com>
2023-05-31 15:46:13 +0200

    Add mk InlineData in TimeSpanHumanizeTests

--- src/Humanizer.Tests.Shared/Localisation/mk/TimeSpanHumanizeTests.cs
+++ src/Humanizer.Tests.Shared/Localisation/mk/TimeSpanHumanizeTests.cs
@@ -8,33 +8,33 @@ namespace Humanizer.Tests.Localisation.mk
     public class TimeSpanHumanizeTests
     {
         [Theory]
-        [InlineData(366, "1 godina")]
-        [InlineData(731, "2 godine")]
-        [InlineData(1096, "3 godine")]
-        [InlineData(4018, "11 godina")]
+        [InlineData(366, "1 година")]
+        [InlineData(731, "2 години")]
+        [InlineData(1096, "3 години")]
+        [InlineData(4018, "11 години")]
         public void Years(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize(maxUnit: Humanizer.Localisation.TimeUnit.Year));
         }
 
         [Theory]
-        [InlineData(31, "1 mjesec")]
-        [InlineData(61, "2 mjeseca")]
-        [InlineData(92, "3 mjeseca")]
-        [InlineData(335, "11 mjeseci")]
+        [InlineData(31, "1 месец")]
+        [InlineData(61, "2 месеци")]
+        [InlineData(92, "3 месеци")]
+        [InlineData(335, "11 месеци")]
         public void Months(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize(maxUnit: Humanizer.Localisation.TimeUnit.Year));
         }
 
         [Theory]
-        [InlineData(1, "1 dan")]
-        [InlineData(2, "2 dana")]
-        [InlineData(3, "3 dana")]
-        [InlineData(4, "4 dana")]
-        [InlineData(5, "5 dana")]
-        [InlineData(7, "1 tjedan")]
-        [InlineData(14, "2 tjedna")]
+        [InlineData(1, "1 ден")]
+        [InlineData(2, "2 дена")]
+        [InlineData(3, "3 дена")]
+        [InlineData(4, "4 дена")]
+        [InlineData(5, "5 дена")]
+        [InlineData(7, "1 недела")]
+        [InlineData(14, "2 недели")]
         public void Days(int days, string expected)
         {
             Assert.Equal(expected, TimeSpan.FromDays(days).Humanize());
