67001dce278792ce8c7b8667eac368edc725b02c
Gjorgji Dimeski <gjorgji.dimeski@codechem.com>
2023-05-31 15:54:22 +0200

    Add Macedonian inline data to DateHumanizeTests

--- src/Humanizer.Tests.Shared/Localisation/mk/DateHumanizeTests.cs
+++ src/Humanizer.Tests.Shared/Localisation/mk/DateHumanizeTests.cs
@@ -8,137 +8,137 @@ namespace Humanizer.Tests.Localisation.mk
     public class DateHumanizeTests
     {
         [Theory]
-        [InlineData(-22, "prije 22 godine")]
-        [InlineData(-5, "prije 5 godina")]
-        [InlineData(-4, "prije 4 godine")]
-        [InlineData(-2, "prije 2 godine")]
-        [InlineData(-1, "prije godinu dana")]
+        [InlineData(-22, "пред 22 години")]
+        [InlineData(-5, "пред 5 години")]
+        [InlineData(-4, "пред 4 години")]
+        [InlineData(-2, "пред 2 години")]
+        [InlineData(-1, "пред година дена")]
         public void YearsAgo(int years, string expected)
         {
             DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Past);
         }
 
         [Theory]
-        [InlineData(5, "za 5 godina")]
-        [InlineData(4, "za 4 godine")]
-        [InlineData(3, "za 3 godine")]
-        [InlineData(2, "za 2 godine")]
-        [InlineData(1, "za godinu dana")]
+        [InlineData(5, "за 5 години")]
+        [InlineData(4, "за 4 години")]
+        [InlineData(3, "за 3 години")]
+        [InlineData(2, "за 2 години")]
+        [InlineData(1, "за година дена")]
         public void YearsFromNow(int years, string expected)
         {
             DateHumanize.Verify(expected, years, TimeUnit.Year, Tense.Future);
         }
 
         [Theory]
-        [InlineData(-8, "prije 8 mjeseci")]
-        [InlineData(-5, "prije 5 mjeseci")]
-        [InlineData(-4, "prije 4 mjeseca")]
-        [InlineData(-3, "prije 3 mjeseca")]
-        [InlineData(-2, "prije 2 mjeseca")]
-        [InlineData(-1, "prije mjesec dana")]
+        [InlineData(-8, "пред 8 месеци")]
+        [InlineData(-5, "пред 5 месеци")]
+        [InlineData(-4, "пред 4 месеци")]
+        [InlineData(-3, "пред 3 месеци")]
+        [InlineData(-2, "пред 2 месеци")]
+        [InlineData(-1, "пред месец дена")]
         public void MonthsAgo(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Past);
         }
 
         [Theory]
-        [InlineData(5, "za 5 mjeseci")]
-        [InlineData(4, "za 4 mjeseca")]
-        [InlineData(2, "za 2 mjeseca")]
-        [InlineData(1, "za mjesec dana")]
+        [InlineData(5, "за 5 месеци")]
+        [InlineData(4, "за 4 месеци")]
+        [InlineData(2, "за 2 месеци")]
+        [InlineData(1, "за месец дена")]
         public void MonthsFromNow(int months, string expected)
         {
             DateHumanize.Verify(expected, months, TimeUnit.Month, Tense.Future);
         }
 
         [Theory]
-        [InlineData(-24, "prije 24 dana")]
-        [InlineData(-22, "prije 22 dana")]
-        [InlineData(-10, "prije 10 dana")]
-        [InlineData(-5, "prije 5 dana")]
-        [InlineData(-4, "prije 4 dana")]
-        [InlineData(-3, "prije 3 dana")]
-        [InlineData(-2, "prije 2 dana")]
-        [InlineData(-1, "jučer")]
+        [InlineData(-24, "пред 24 дена")]
+        [InlineData(-22, "пред 22 дена")]
+        [InlineData(-10, "пред 10 дена")]
+        [InlineData(-5, "пред 5 дена")]
+        [InlineData(-4, "пред 4 дена")]
+        [InlineData(-3, "пред 3 дена")]
+        [InlineData(-2, "пред 2 дена")]
+        [InlineData(-1, "вчера")]
         public void DaysAgo(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Past);
         }
 
         [Theory]
-        [InlineData(10, "za 10 dana")]
-        [InlineData(5, "za 5 dana")]
-        [InlineData(1, "sutra")]
+        [InlineData(10, "за 10 дена")]
+        [InlineData(5, "за 5 дена")]
+        [InlineData(1, "утре")]
         public void DaysFromNow(int days, string expected)
         {
             DateHumanize.Verify(expected, days, TimeUnit.Day, Tense.Future);
         }
 
         [Theory]
-        [InlineData(-10, "prije 10 sati")]
-        [InlineData(-5, "prije 5 sati")]
-        [InlineData(-4, "prije 4 sata")]
-        [InlineData(-3, "prije 3 sata")]
-        [InlineData(-2, "prije 2 sata")]
-        [InlineData(-1, "prije sat vremena")]
+        [InlineData(-10, "пред 10 часа")]
+        [InlineData(-5, "пред 5 часа")]
+        [InlineData(-4, "пред 4 часа")]
+        [InlineData(-3, "пред 3 часа")]
+        [InlineData(-2, "пред 2 часа")]
+        [InlineData(-1, "пред 1 час")]
         public void HoursAgo(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Past);
         }
 
         [Theory]
-        [InlineData(5, "za 5 sati")]
-        [InlineData(4, "za 4 sata")]
-        [InlineData(3, "za 3 sata")]
-        [InlineData(2, "za 2 sata")]
-        [InlineData(1, "za sat vremena")]
+        [InlineData(5, "за 5 часа")]
+        [InlineData(4, "за 4 часа")]
+        [InlineData(3, "за 3 часа")]
+        [InlineData(2, "за 2 часа")]
+        [InlineData(1, "за 1 час")]
         public void HoursFromNow(int hours, string expected)
         {
             DateHumanize.Verify(expected, hours, TimeUnit.Hour, Tense.Future);
         }
 
         [Theory]
-        [InlineData(-10, "prije 10 minuta")]
-        [InlineData(-5, "prije 5 minuta")]
-        [InlineData(-4, "prije 4 minute")]
-        [InlineData(-3, "prije 3 minute")]
-        [InlineData(-2, "prije 2 minute")]
-        [InlineData(-1, "prije jedne minute")]
+        [InlineData(-10, "пред 10 минути")]
+        [InlineData(-5, "пред 5 минути")]
+        [InlineData(-4, "пред 4 минути")]
+        [InlineData(-3, "пред 3 минути")]
+        [InlineData(-2, "пред 2 минути")]
+        [InlineData(-1, "пред минута")]
         public void MinutesAgo(int minutes, string expected)
         {
             DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Past);
         }
 
         [Theory]
-        [InlineData(5, "za 5 minuta")]
-        [InlineData(4, "za 4 minute")]
-        [InlineData(3, "za 3 minute")]
-        [InlineData(2, "za 2 minute")]
-        [InlineData(1, "za jednu minutu")]
+        [InlineData(5, "за 5 минути")]
+        [InlineData(4, "за 4 минути")]
+        [InlineData(3, "за 3 минути")]
+        [InlineData(2, "за 2 минути")]
+        [InlineData(1, "за минута")]
         public void MinutesFromNow(int minutes, string expected)
         {
             DateHumanize.Verify(expected, minutes, TimeUnit.Minute, Tense.Future);
         }
 
         [Theory]
-        [InlineData(-10, "prije 10 sekundi")]
-        [InlineData(-5, "prije 5 sekundi")]
-        [InlineData(-4, "prije 4 sekunde")]
-        [InlineData(-3, "prije 3 sekunde")]
-        [InlineData(-2, "prije 2 sekunde")]
-        [InlineData(-1, "prije jedne sekunde")]
+        [InlineData(-10, "пред 10 секунди")]
+        [InlineData(-5, "пред 5 секунди")]
+        [InlineData(-4, "пред 4 секунди")]
+        [InlineData(-3, "пред 3 секунди")]
+        [InlineData(-2, "пред 2 секунди")]
+        [InlineData(-1, "пред секунда")]
         public void SecondsAgo(int seconds, string expected)
         {
             DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Past);
         }
 
         [Theory]
-        [InlineData(10, "za 10 sekundi")]
-        [InlineData(5, "za 5 sekundi")]
-        [InlineData(4, "za 4 sekunde")]
-        [InlineData(3, "za 3 sekunde")]
-        [InlineData(2, "za 2 sekunde")]
-        [InlineData(1, "za jednu sekundu")]
+        [InlineData(10, "за 10 секунди")]
+        [InlineData(5, "за 5 секунди")]
+        [InlineData(4, "за 4 секунди")]
+        [InlineData(3, "за 3 секунди")]
+        [InlineData(2, "за 2 секунди")]
+        [InlineData(1, "за секунда")]
         public void SecondsFromNow(int seconds, string expected)
         {
             DateHumanize.Verify(expected, seconds, TimeUnit.Second, Tense.Future);
